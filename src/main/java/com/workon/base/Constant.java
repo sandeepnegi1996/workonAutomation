61a8454998228be92b2ceb44406322ffda820e1b
sandeepnegi1996 <9041128179sandy@gmail.com>
2020-06-27 09:33:33 +0530

    added the function init config and quit browser in the Page.java class and also added some constants in the constant file

--- src/main/java/com/workon/base/Constant.java
+++ src/main/java/com/workon/base/Constant.java
@@ -1,5 +1,12 @@
 package com.workon.base;
 
 public class Constant {
+	
+	public static final String dashboardPUrl="";
+	public static final String dashboardQUrl="";
+	public static final String dashboardDUrl="";
+	public static final String chromeBrowser="chrome";
+	public static final String firefoxBrowser="";
+	public static final int waitingTime=15;
 
 }
--- src/main/java/com/workon/base/Page.java
+++ src/main/java/com/workon/base/Page.java
@@ -1,5 +1,55 @@
 package com.workon.base;
 
-public class Page {
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Page {
+	
+	public static WebDriver driver=null;
+
+	// initConfiguration
+	public  void initConfiguration() {
+		
+		if(driver==null) {
+			
+			//webdrivermanager
+			WebDriverManager.chromedriver().setup();
+			//webdriver
+			
+			driver.get(url);
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		}
+	}
+
+
+	// quitBrowser
+	public void quitBrowser() {
+		
+		if(driver!=null) {
+			driver.quit();
+		}
+	}
+	
+
+	// click
+
+	public static void click(WebElement element) {
+		element.click();
+		System.out.println("Element is clicked "+element.getText());
+	}
+
+	// type
+
+	
+	  public static void type(WebElement element,String inputString) {
+		  element.sendKeys(inputString);
+		  
+		  System.out.println("String is entered in the element"+element.getText());
+	  }
+	 
 }
