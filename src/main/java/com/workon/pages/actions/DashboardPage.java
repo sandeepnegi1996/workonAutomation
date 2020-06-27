731550c06ae9c1daea769f040cb901efcb132e25
sandeepnegi1996 <9041128179sandy@gmail.com>
2020-06-27 11:29:41 +0530

    Basic structure of dashboard page is finished

--- src/main/java/com/workon/pages/actions/DashboardPage.java
+++ src/main/java/com/workon/pages/actions/DashboardPage.java
@@ -1,5 +1,74 @@
 package com.workon.pages.actions;
 
-public class DashboardPage {
+import com.workon.base.Page;
+import com.workon.pages.locators.DashboardPageLocators;
+
+public class DashboardPage extends Page {
+	
+	//DashboardPage is a Page so thats why we are extending the Page 
+	
+	
+	public static DashboardPageLocators dashboardpagelocator;
+	
+	
+	public DashboardPage() {
+		this.dashboardpagelocator=new DashboardPageLocators();
+		
+		
+	}
+	
+	
+	/*
+	 * in some cases i am using directly gotoCentralSearch like this 
+	 * but in some cases i am using gotoworkonArchive i am using workon in the function name
+	 * this is because of the name in the workon dashboard page
+	 * 
+	 */
+	
+	public void gotoSearchBox() {
+		
+	}
+	
+	public void gotoCentralSearch() {
+		
+	}
+	
+	public void gotoWorkONArchive() {
+		
+		
+	}
+	
+	public void gotoUIB() {
+		
+	}
+	
+	public void gotoWorkONProfile() {
+		
+	}
+	
+	public void gotoDevQualityCheckTool() {
+		
+	}
+	
+	public void gotoWorkONEndUserWiki() {
+		
+	}
+	
+	public void gotoWorkONCommunity() {
+		
+	}
+	
+	public void gotoWorkONGlobalNet() {
+		
+	}
+	
+	public void gotoUserInformation() {
+		
+		
+	}
+	
+	
+	
+	
 
 }
